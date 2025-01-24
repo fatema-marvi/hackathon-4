a13d1bc32d5fbfe73fa2e99049594f19f8043d93
fatema-marvi <160221766+fatema-marvi@users.noreply.github.com>
2025-01-24 15:57:05 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 "use client";
 import localFont from "next/font/local";
 import "./globals.css";
-import TopHeader from "./components/topheader/topheader"
 import Footer from "./components/footer/footer";
 import { CartProvider } from "./components/context/cartContext";
-import Navbar from "./components/navbar/navbar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,12 +21,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <CartProvider> 
-          <TopHeader /> {/* Top Header at the top */}
-          <Navbar onSearch={function (query: string): void {
+           {/* <TopHeader />  */}
+          {/* <Navbar onSearch={function (query: string): void {
             throw new Error("Function not implemented.");
-          } }/>
-          <main className="mt-[150px] px-4 sm:px-6 lg:px-8">{children}</main>  {/* Only one instance of {children}, with responsive padding */}
-          <Footer /> {/* Footer at the bottom */}
+          } }/> */}
+          <main className="mt-[150px] px-4 sm:px-6 lg:px-8">{children}</main>  
+           <Footer /> {/* Footer at the bottom */}
         </CartProvider>
       </body>
     </html>
